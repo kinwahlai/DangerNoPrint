2e6417e74ea9e8477b65914ce3f8e54e3110bd95
Darren <darren.lai@deem.com>
2020-03-07 16:41:58 +0000

    Unhide dependencies

--- Package.swift
+++ Package.swift
@@ -10,18 +10,18 @@ let package = Package(
         .library(
             name: "DangerNoPrint",
             targets: ["DangerNoPrint"]),
-//        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
+        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/danger/swift.git", from: "3.0.0"),
-//        .package(url: "https://github.com/f-meloni/Rocket", from: "1.0.0"), // dev
+        .package(url: "https://github.com/f-meloni/Rocket", from: "1.0.0"), // dev
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-//        .target(name: "DangerDependencies",dependencies: ["Danger", "DangerNoPrint"]), //dev
+        .target(name: "DangerDependencies",dependencies: ["Danger", "DangerNoPrint"]), //dev
         .target(
             name: "DangerNoPrint",
             dependencies: ["Danger"]),
